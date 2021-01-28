e30776cc19562ed1522d7151cbcb3b9bb530388f
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-01-28 07:32:26 +0100

    Ex 1 Celsius to Farenheit

--- 1 - Conversion/app.js
+++ 1 - Conversion/app.js
@@ -7,17 +7,3 @@ function conversionCelsiusToFareinheit(degreesCelsius) {
     return console.log('Données en entrée non correctes.')
   }
 }
-
-// console.log(conversionCelsiusToFareinheit(50))
-
-// ----------------- ÉNONCÉ -----------------
-
-// Créer un algorithme qui sert à convertir des degrés Celsius
-// en degrés Fahrenheit.
-// Le résultat doit être de type nombre.
-// Si on donne un autre type qu'un nombre en argument, vous
-// devez retourner "Données en entrée non correctes."
-// Bonne Chance !
-
-// Convertir un degrés Celsius en Fahrenheit
-// La formule : Celsius * 9/5 + 32
